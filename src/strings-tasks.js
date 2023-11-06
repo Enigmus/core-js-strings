5e243ef4d7cb11fbb66235a602be940500d1f358
sd <sd_net@mail.ru>
2023-11-06 22:53:36 +0300

    feat: add fn findLongestWord()

--- src/strings-tasks.js
+++ src/strings-tasks.js
@@ -343,9 +343,12 @@ function isPalindrome(str) {
  *   findLongestWord('A long and winding road') => 'winding'
  *   findLongestWord('No words here') => 'words'
  */
-function findLongestWord(/* sentence */) {
-  throw new Error('Not implemented');
+function findLongestWord(sentence) {
+  const arr = sentence.split(' ').sort((a, b) => b.length - a.length);
+  return arr[0];
 }
+
+// console.log(findLongestWord('A long and winding road'));
 
 /**
  * Returns the string where each word is reversed.

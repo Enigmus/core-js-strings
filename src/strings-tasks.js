76a0e5635f2af34cca5c48d6c3c4f8a100aa697d
sd <sd_net@mail.ru>
2023-11-06 23:05:36 +0300

    feat: add fn invertCase()

--- src/strings-tasks.js
+++ src/strings-tasks.js
@@ -380,8 +380,13 @@ function reverseWords(str) {
  *   invertCase('JavaScript is Fun') => 'jAVASCRIPT IS fUN'
  *   invertCase('12345') => '12345'
  */
-function invertCase(/* str */) {
-  throw new Error('Not implemented');
+function invertCase(str) {
+  let s = '';
+  for (let i = 0; i < str.length; i += 1) {
+    if (str[i] === str[i].toLowerCase()) s += str[i].toUpperCase();
+    else s += str[i].toLowerCase();
+  }
+  return s;
 }
 
 /**
